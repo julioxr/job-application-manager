740922487343896a0bb4e2d29298f16788925f70
julio <julioroja987@gmail.com>
2023-06-15 22:35:52 -0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <main>
+                <h1>Resume Manager</h1>
+            </main>
+        </>
+    );
+}
+
+export default App;
